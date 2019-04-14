e9d7f67ae84d9bce37d8fbc218ef805e80966485
mi-h <>
2019-04-14 23:17:15 +0900

    resigned folder

--- app/src/main/java/com/activity/mih916/qiitaproject/view/activity/MainActivity.kt
+++ app/src/main/java/com/activity/mih916/qiitaproject/view/activity/MainActivity.kt
@@ -1,18 +1,19 @@
-package com.activity.mih916.qiitaproject
+package com.activity.mih916.qiitaproject.view.activity
 
 import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.activity.mih916.qiitaproject.R
 import com.activity.mih916.qiitaproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     val binding:ActivityMainBinding by lazy {
-        DataBindingUtil.setContentView<ActivityMainBinding>(this,R.layout.activity_main)
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding.text.text = "text_change"
+        binding.text.text = "file_changed"
     }
 }
