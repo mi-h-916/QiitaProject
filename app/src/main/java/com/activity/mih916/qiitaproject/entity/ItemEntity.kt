32bbd11a996a153add4f2434381b2ffb9f82a45a
mi-h <>
2019-04-28 21:30:08 +0900

    add_api_element

--- app/src/main/java/com/activity/mih916/qiitaproject/entity/ItemEntity.kt
+++ app/src/main/java/com/activity/mih916/qiitaproject/entity/ItemEntity.kt
@@ -8,5 +8,13 @@ data class ItemEntity (
         @Json(name = "title")
         val title: String,
         @Json(name = "body")
-        val body: String
+        val body: String,
+        @Json(name = "comments_count")
+        val comments_count: Int,
+        @Json(name = "likes_count")
+        val likes_count: Int,
+        @Json(name = "updated_at")
+        val update_time: String,
+        @Json(name = "user")
+        val user: UserEntity
 )
